78c6ffe4b49377bcef7251078a87b26f9d19d1ec
Nguyen Son Tung <nguyensontung@gobiz.vn>
2023-05-12 15:16:53 +0700

    Update

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    debug: process.env.APP_DEBUG,
+
     dingTalk: {
         token: process.env.DING_TALK_TOKEN,
         keyword: process.env.DING_TALK_KEYWORK,
--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -1,7 +1,12 @@
 const lodash = require('lodash');
 const {notification} = require('../services');
+const config = require('../config');
 
 exports.sentry = (req, res) => {
+    if (config.debug) {
+        console.log('sentry', JSON.stringify(req.body));
+    }
+
     let project = lodash.toUpper(req.body.project);
     let url = req.body.url;
     let message = lodash.get(req.body, 'event.metadata.value') || req.body.title;
